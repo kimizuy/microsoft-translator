685b2085c27c2826311f1e73b14be5908d4b183d
kimizuy <kimizuyamasaki@gmail.com>
2024-06-23 02:08:07 -0700

    fix document

--- mod.ts
+++ mod.ts
@@ -4,14 +4,15 @@
  *
  * @example
  * ```ts
- * import { translate } from "@kimizuy/translate";
+ * import { translate } from "@kimizuy/microsoft-translator"
  *
- * const translator = new Translator(YOUR_API_KEY, RESION);
+ * const translator = new Translator(YOUR_API_KEY, REGION);
  * const result = await translator.translate({
  *   texts: "Hello",
  *   to: "ja",
  *   from: "en",
  * });
+ *
  * console.log(result?.[0].translations[0].text); // こんにちは
  * ```
  *
