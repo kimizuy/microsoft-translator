bcc38f2499f892408092d40be0500b3568ad5594
kimizuy <kimizuyamasaki@gmail.com>
2024-06-23 02:45:45 -0700

    add jsdoc

--- translate.ts
+++ translate.ts
@@ -1,34 +1,124 @@
+/**
+ * The base URL of the Azure Translator API.
+ *
+ * @type {"https://api.cognitive.microsofttranslator.com/translate?api-version=3.0"}
+ */
 const BASE_URL =
   "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0";
 
-interface TranslateOptions {
+/**
+ * The options for the translation.
+ *
+ * @export
+ * @interface TranslateOptions
+ * @typedef {TranslateOptions}
+ */
+export interface TranslateOptions {
+  /**
+   * The text to translate.
+   *
+   * @type {(string | string[])}
+   */
   texts: string | string[];
+  /**
+   * The language code of the translated text.
+   *
+   * @type {string}
+   */
   to: string;
+  /**
+   * The language code of the text.
+   *
+   * @type {?string}
+   */
   from?: string;
+  /**
+   * The type of the text.
+   *
+   * @type {?("plain" | "html")}
+   */
   textType?: "plain" | "html";
 }
 
-type Result = {
+/**
+ * The result of the translation.
+ *
+ * @export
+ * @typedef {Result}
+ */
+export type Result = {
   translations: { text: string; to: string }[];
   detectedLanguage?: { language: string; score: number };
 }[];
 
-interface ErrorResponse {
+/**
+ * The error response from the API.
+ *
+ * @export
+ * @interface ErrorResponse
+ * @typedef {ErrorResponse}
+ */
+export interface ErrorResponse {
+  /**
+   * The error code and message.
+   *
+   * @type {{
+   *     code: number;
+   *     message: string;
+   *   }}
+   */
   error: {
     code: number;
     message: string;
   };
 }
 
+/**
+ * A class to translate languages.
+ *
+ * @export
+ * @class Translator
+ * @typedef {Translator}
+ */
 export class Translator {
+  /**
+   * The API key of the Azure service.
+   *
+   * @private
+   * @type {string}
+   */
   private apiKey: string;
+  /**
+   * The region of the Azure service.
+   *
+   * @private
+   * @type {string}
+   */
   private region: string;
 
+  /**
+   * Creates an instance of Translator.
+   *
+   * @constructor
+   * @param {string} apiKey
+   * @param {string} region
+   */
   constructor(apiKey: string, region: string) {
     this.apiKey = apiKey;
     this.region = region;
   }
 
+  /**
+   * Translates the given texts.
+   *
+   * @async
+   * @param {TranslateOptions} param0
+   * @param {(string | {})} param0.texts
+   * @param {string} param0.to
+   * @param {string} param0.from
+   * @param {("plain" | "html")} [param0.textType="plain"]
+   * @returns {Promise<Result | null>}
+   */
   async translate({
     texts,
     to,
