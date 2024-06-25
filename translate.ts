4a9220c8a1035fc780fc63ba7d2b3163ee7f0b94
kimizuy <kimizuyamasaki@gmail.com>
2024-06-25 01:23:33 -0700

    reset jsdoc & rename type

--- translate.ts
+++ translate.ts
@@ -1,111 +1,36 @@
 import { BASE_URL, type SupportedLanguage } from "./constants.ts";
 
-/**
- * The options for the translation.
- *
- * @export
- * @interface Options
- * @typedef {Options}
- */
 export interface Options {
-  /**
-   * The language to translate from.
-   *
-   * @type {?SupportedLanguage}
-   */
   from?: SupportedLanguage;
-  /**
-   * The type of the text.
-   *
-   * @type {?("plain" | "html")}
-   */
   textType?: "plain" | "html";
 }
 
-/**
- * The result of the translation.
- *
- * @export
- * @typedef {Result}
- */
-export type Result = {
+export type Success = {
   translations: { text: string; to: string }[];
   detectedLanguage?: { language: string; score: number };
 }[];
 
-/**
- * The error response from the API.
- *
- * @export
- * @interface ErrorResponse
- * @typedef {ErrorResponse}
- */
-export interface ErrorResponse {
-  /**
-   * The error code and message.
-   *
-   * @type {{
-   *     code: number;
-   *     message: string;
-   *   }}
-   */
+export interface Failure {
   error: {
     code: number;
     message: string;
   };
 }
 
-/**
- * A class to translate languages.
- *
- * @export
- * @class Translator
- * @typedef {Translator}
- */
 export class Translator {
-  /**
-   * The API key of the Azure service.
-   *
-   * @private
-   * @type {string}
-   */
   private apiKey: string;
-  /**
-   * The region of the Azure service.
-   *
-   * @private
-   * @type {string}
-   */
   private region: string;
 
-  /**
-   * Creates an instance of Translator.
-   *
-   * @constructor
-   * @param {string} apiKey
-   * @param {string} region
-   */
   constructor(apiKey: string, region: string) {
     this.apiKey = apiKey;
     this.region = region;
   }
 
-  /**
-   * Translates the given texts to the specified language.
-   *
-   * @async
-   * @param {(string | string[])} texts
-   * @param {SupportedLanguage} to
-   * @param {Options} param0
-   * @param {SupportedLanguage} param0.from
-   * @param {("plain" | "html")} [param0.textType="plain"]
-   * @returns {Promise<Result | null>}
-   */
   async translate(
     texts: string | string[],
     to: SupportedLanguage,
     options?: Options
-  ): Promise<Result | null> {
+  ): Promise<Success | null> {
     const url = new URL(BASE_URL);
     url.searchParams.set("to", to);
     if (options?.from) {
@@ -130,7 +55,7 @@ export class Translator {
         body,
       });
 
-      const result: Result | ErrorResponse = await response.json();
+      const result: Success | Failure = await response.json();
 
       if ("error" in result) {
         throw new Error(`${result.error.code}: ${result.error.message}`);
